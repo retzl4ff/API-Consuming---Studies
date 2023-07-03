432e75e5d522b53c257e126b3f5ff48d7324b0e8
retzl4ff <joaoaretzlaff@gmail.com>
2023-07-02 22:38:52 -0300

    Last commit

---
+++ fetch/main.js
@@ -0,0 +1,76 @@
+const url = "http://localhost:5500/api"
+
+function getUsers(){
+    fetch(url)
+    .then(response => response.json())
+    .then(data => renderApiResult.textContent = JSON.stringify(data))
+    .catch(error => console.error(error))
+}
+
+function getUser(id){
+    fetch(`${url}/${id}`)
+    .then(response => response.json())
+    .then(data => {
+        userName.textContent = data.name
+        userCity.textContent = data.city
+        userAvatar.src = data.avatar
+    })
+    .catch(error => console.error(error))
+}
+
+function addUser(newUser){
+    fetch(url, {
+        method: "POST",
+        body: JSON.stringify(newUser),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    })
+    .then(response => response.json())
+    .then(data => alertApi.textContent = data)
+    .catch(error => console.error(error))
+}
+
+function updateUser(updatedUser, id){
+    fetch(`${url}/${id}`, {
+        method: "PUT",
+        body: JSON.stringify(updatedUser),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    })
+    .then(response => response.json())
+    .then(data => alertApi.textContent = data)
+    .catch(error => console.error(error))
+}
+
+function deleteUser(id){
+    fetch(`${url}/${id}`, {
+        method: "DELETE",
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    })
+    .then(response => response.json())
+    .then(data => alertApi.textContent = data)
+    .catch(error => console.error(error))
+}
+
+const newUser = {
+    name: "João Alexandre",
+    avatar: "http://lorempixel.com.br/500/400/?2",
+    city: "Joinville",
+}
+
+const updatedUser = {
+    name: "Mayk Brito",
+    avatar: "http://lorempixel.com.br/500/400/?2",
+    city: "Paraná"
+}
+
+deleteUser(2)
+getUsers()
+getUser(9)
+// addUser(newUser)
+// updateUser(updatedUser, 9)
+
